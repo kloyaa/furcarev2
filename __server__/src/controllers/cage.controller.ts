fe722bb667b656e40548d07c386c78665dda33d1
Kolya <madridano.kolya@gmail.com>
2024-02-21 15:33:16 +0800

    init cage and schedule controllers

---
+++ __server__/src/controllers/cage.controller.ts
@@ -0,0 +1,5 @@
+/**
+ * TODO 
+ * Create an Endpoint that returns all the cages with availability count
+ * the availability will be based on current bookings that uses the cage id
+ */
---
+++ __server__/src/controllers/schedules.controller.ts
@@ -0,0 +1,6 @@
+/**
+ * TODO 
+ * Create an Endpoint that returns all the schedules with availability status
+ * and the status will be based if there is a booking that uses the schedule id and the
+ * booking status is not yet 'cancelled', 'completed' or 'done'
+ */
